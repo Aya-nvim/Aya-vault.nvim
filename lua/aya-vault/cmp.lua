0fa592a5889070ac1a8439109123a23c692f7b9d
yqwu905 <pingline@outlook.com>
2023-06-23 15:02:07 +0800

    feat(cmp): Nvim-cmp integration

--- lua/aya-vault/cmp.lua
+++ lua/aya-vault/cmp.lua
@@ -1,10 +1,10 @@
-print("SADjio")
 local has_cmp, cmp = pcall(require, "cmp")
+local index = require("aya-vault.index")
 
--- if not has_cmp then
---     vim.notify("nvim-cmp not found!", vim.log.levels.ERROR)
---     return
--- end
+if not has_cmp then
+    vim.notify("nvim-cmp not found!", vim.log.levels.ERROR)
+    return
+end
 
 local source = {}
 
@@ -12,44 +12,32 @@ source.new = function()
     return setmetatable({}, { __index = source })
 end
 
-source.get_trigger_characters = function()
-    return { "[" }
-end
-
 source.complete = function(self, request, callback)
     local items = {}
-    local input = string.sub(request.context.cursor_before_line, request.offset - 1)
     local prefix = string.sub(request.context.cursor_before_line, 1, request.offset - 1)
-    print(input)
-    print(prefix)
-    local handle = "abc"
-    local name_and_email = "def"
-    table.insert(items, {
-        filterText = handle .. " " .. name_and_email,
-        label = name_and_email,
-        textEdit = {
-            newText = name_and_email,
-            range = {
-                start = {
-                    line = request.context.cursor.row - 1,
-                    character = request.context.cursor.col - 1 - #input,
-                },
-                ["end"] = {
-                    line = request.context.cursor.row - 1,
-                    character = request.context.cursor.col - 1,
-                },
-            },
-        },
-    })
-    callback({
-        items = items,
-        isIncomplete = true,
-    })
+
+    local files = index.get_aya_file(vim.fn.getcwd())
+    if vim.endswith(prefix, "[[") then
+        for _, file in ipairs(files) do
+            table.insert(items, {
+                filterText = "[[" .. file,
+                label = file,
+                kind = cmp.lsp.CompletionItemKind.File,
+            })
+        end
+        callback({
+            items = items,
+            isIncomplete = true,
+        })
+    else
+        callback({
+            isIncomplete = true,
+        })
+    end
 end
 
 return {
     setup = function()
-        print("Setup cmp")
         cmp.register_source("aya-vault", source.new())
         cmp.setup.filetype("markdown", {
             sources = cmp.config.sources({
